61044062387ac9b91770cc636bed64268c476967
AS-Aurora <aayushsharma.lucky97@gmail.com>
2025-02-27 20:28:00 +0530

    Added the socket.io part

---
+++ server.js
@@ -0,0 +1,31 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New connection", socket.id);
+
+  socket.on("join-room", ({ username, room }) => {
+    console.log("Joining room", room, username);
+    socket.join(room);
+
+    socket.to(room).emit("message", {
+      sender: "System",
+      message: `${username} has joined the room`,
+    });
+  });
+
+  socket.on("sendMessage", ({ message, username, room }) => {
+    io.to(room).emit("message", { sender: username, message });
+  });
+});
+
+httpServer.listen(5000, () => {
+  console.log("Server listening on port 5000");
+});
