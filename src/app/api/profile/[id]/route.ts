fd5d1e3ed3c913efc8e4513d1222bf4ca0ee420f
AS-Aurora <aayushsharma.lucky97@gmail.com>
2025-03-10 19:47:12 +0530

    Added the profile page api

--- src/app/api/profile/[id]/route.ts
+++ src/app/api/profile/[id]/route.ts
@@ -1,24 +1,57 @@
 import { connect } from "@/src/dbconfig/dbconfig";
 import User from "@/src/models/userModel";
-import {NextRequest, NextResponse} from "next/server";
+import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+import { use } from "react";
 
 
 connect()
 
-export async function GET(request: NextRequest, {params}: {params: {id: string}}) {
-    try {
-        const reqbody = await request.json()
-        const {username} = reqbody
-        const user = await User.findById(params.id).select('-password -otp')
 
-        if(!user) {
-            return NextResponse.json({message: 'User not found'}, {status: 404})
+export async function GET(request: NextRequest, context: { params: Promise<{ id: string }> }) {
+    const params = use(context.params)
+    const { id } = params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return NextResponse.json({ message: "Invalid ID" }, { status: 400 });
+    }
+    try {
+        const user = await User.findById(id).select("-password -otp");
+
+        if (!user) {
+            return NextResponse.json({ message: "User not found" }, { status: 404 })
         }
 
-        return NextResponse.json(user, {status: 200})
-        
+        return NextResponse.json(user, { status: 200 });
+
     } catch (error: any) {
-        return NextResponse.json({message: error.message}, {status: 500})
+        return NextResponse.json({ message: error.message }, { status: 500 });
     }
 }
 
+
+export async function PUT(request: NextRequest, context: { params: { id: string } }) {
+    const { id } = context.params
+    try {
+        const reqBody = await request.json();
+        const { username } = reqBody;
+
+        if (!username) {
+            return NextResponse.json({ message: "Username required" }, { status: 400 });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            { username },
+            { new: true, runValidators: true }
+        ).select("-password -otp");
+
+        if (!updatedUser) {
+            return NextResponse.json({ message: "User not found" }, { status: 404 });
+        }
+
+        return NextResponse.json(updatedUser, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
